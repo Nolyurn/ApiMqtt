a6c759da09ec71a00f0a20b1e0a7aeab6822ea1a
alekshar <alekshar@github.com>
2017-02-06 12:10:29 +0100

    api test improvement

--- api/test/admin.js
+++ api/test/admin.js
@@ -1,124 +1,96 @@
 import expect from 'expect';
 import Admin from '../modules/Admin.js';
 import {Privilege} from '../modules/Admin.js';
+var mqtt = require('mqtt');
 
-var server = require('../../services/mqtt/server/server.js').mqttServ;
+var server = require('./test-server.js').server;
+
+// mosca broker binding
+server.on('published', function(packet, client) {
+    if(packet.topic !== "admin/createUser"
+        && packet.topic !== "admin/deleteUser"){
+        return;
+    }
+    
+    var payload = JSON.parse(packet.payload);
+    if(packet.topic === "admin/createUser" 
+            && typeof payload.password == "undefined"){
+        return;
+    }
+    if(packet.topic === "admin/deleteUser" 
+            && typeof payload.password != "undefined"){
+        return;
+    }
+
+    var response = {
+        token: payload.token
+    };
+    if(payload.username === "succeed"){
+        response.success = true;
+    } else {
+        response.success = false;
+    }
+    server.publish({topic:"admin/event",payload:JSON.stringify(response)});
+});
 
 var admin; 
 var mqttUrl = "mqtt://localhost:1883";
-var simpleUser = {
-		username:"test",
-		password:"test"
-};
 var adminUser = {
-		username:"test",
-		password:"test"
+        username:"test",
+        password:"test"
 };
 
 describe('Admin test', function() {
-	describe('user management', function() {
-		before(function(done){
-			admin = new Admin(mqttUrl, adminUser);
-			admin.on('connect', function(){
-				done();
-			});
-			admin.on('error', function(){
-				done("Failed to initialize tests");
-			});
-			
-		});
-		it("createUser failed : user already exists", function(done){
-			admin.createUser("cuf_uae", "test", Privilege.CLIENT, {
-				onSuccess: function(){
-					admin.createUser("cuf_uae", "test", Privilege.CLIENT, {
-						onSuccess: function(){
-							done("creating the same user twice should fail");
-						},
-						onError: function(){
-							done();
-						}
-					});
-				},
-				onError: function(){
-					done("first creation failed, something is wrong in test script");
-				}
-			});
-		});
-		it("createUser failed : name undefined", function(done){
-			admin.createUser("", "test", Privilege.CLIENT, {
-				onSuccess: function(){
-					done("creating the same user twice should fail");
-				},
-				onError: function(){
-					done();
-				}
-			});
-		});
-		it("createUser failed : not an admin account", function(done){
-			var client = new Admin(mqttUrl, simpleUser);
-			client.createUser("test", "test", Privilege.CLIENT, {
-				onSuccess: function(){
-					done("create a user without being admin should fail");
-				},
-				onError: function(){
-					done();
-				}
-			});
-		});
-		it("createUser successed", function(done){
-			admin.createUser("cus", "password", Privilege.CLIENT, {
-				onSuccess: function(){
-					done();
-				},
-				onError: function(){
-					done("Creation failed");
-				}
-			});
-		});
-		it("deleteUser failed : user does not exist", function(done){
-			admin.deleteUser("duf_udne", "password", Privilege.CLIENT, {
-				onSuccess: function(){
-					done("Deleting an inexistant user should fail");
-				},
-				onError: function(){
-					done();
-				}
-			});
-		});
-		it("deleteUser successed", function(done){
-			admin.createUser("dus", "test", Privilege.CLIENT, {
-				onSuccess: function(){
-					admin.deleteUser("dus", {
-						onSuccess: function(){
-							done();
-						},
-						onError: function(){
-							done("cannot delete the user");
-						}
-					});
-				},
-				onError: function(){
-					done("first creation failed, something is wrong in test script");
-				}
-			});
-		});
-		it("deleteUser failed : not an admin account", function(done){
-			var client = new Admin(mqttUrl, simpleUser);
-			admin.createUser("test", "test", Privilege.CLIENT, {
-				onSuccess: function(){
-					client.deleteUser("test", {
-						onSuccess: function(){
-							done("delete a user without being admin should fail");
-						},
-						onError: function(){
-							done();
-						}
-					});
-				},
-				onError: function(){
-					done("first creation failed, something is wrong in test script");
-				}
-			});
-		});
-	});
+    describe('user management', function() {
+        before(function(done){
+            admin = new Admin(mqttUrl, adminUser);
+            admin.on('connect', function(){
+                done();
+            });
+            admin.on('error', function(){
+                done("Failed to initialize tests");
+            });
+            
+        });
+        it("createUser failed", function(done){
+            admin.createUser("fail", "password", Privilege.CLIENT, {
+                onSuccess: function(){
+                    done("createUser should call onError callback");
+                },
+                onError: function(){
+                    done();
+                }
+            });
+        });
+        it("createUser succeed", function(done){
+            admin.createUser("succeed", "password", Privilege.CLIENT, {
+                onSuccess: function(){
+                    done();
+                },
+                onError: function(){
+                    done("createUser should call onSuccess callback");
+                }
+            });
+        });
+        it("deleteUser failed", function(done){
+            admin.deleteUser("fail", Privilege.CLIENT, {
+                onSuccess: function(){
+                    done("deleteUser should call onError callback");
+                },
+                onError: function(){
+                    done();
+                }
+            });
+        });
+        it("deleteUser succeed", function(done){
+            admin.deleteUser("succeed", {
+                onSuccess: function(){
+                    done();
+                },
+                onError: function(){
+                    done("deleteUser should call onSuccess callback");
+                }
+            });
+        });
+    });
 });
--- api/test/moderator.js
+++ api/test/moderator.js
@@ -2,176 +2,107 @@ import expect from 'expect';
 import Moderator from '../modules/Moderator.js';
 import {Types} from '../modules/Moderator.js';
 
-var server = require('../../services/mqtt/server/server.js').mqttServ;
+var server = require('./test-server.js').server;
+
+// mosca broker binding
+server.on('published', function(packet, client) {
+    if(packet.topic !== "sensor/start"
+        && packet.topic !== "sensor/stop"){
+        return;
+    }
+    
+    var payload = JSON.parse(packet.payload);
+
+    var response = {
+        token: payload.token
+    };
+    var shallSucceed;
+    switch(packet.topic){
+    case "sensor/start":
+        shallSucceed = payload.toSend.name;
+        break;
+    case "sensor/stop":
+        shallSucceed = payload.sensor;
+        break;
+    }
+    if(shallSucceed === "succeed"){
+        response.success = true;
+    } else {
+        response.success = false;
+    }
+    server.publish({topic:"sensor/event",payload:JSON.stringify(response)});
+});
 
 var moderator; 
 var mqttUrl = "mqtt://localhost:1883";
 var simpleUser = {
-		username:"test",
-		password:"test"
+        username:"test",
+        password:"test"
 };
 var moderatorUser = {
-		username:"test",
-		password:"test"
+        username:"test",
+        password:"test"
 };
 
 describe('Moderator test', function() {
-	describe('sensor management', function() {
-		before(function(done){
-			moderator = new Moderator(mqttUrl, moderatorUser);
-			moderator.on('connect', function(){
-				done();
-			});
-			moderator.on('error', function(){
-				done("Failed to initialize tests");
-			});
-		});
-		it("createSensor failed : sensor already exists", function(done){
-			var sensor = {
-				name:"csf_sae",
-				type:Types.POSITIVE_NUMBER,
-				freq:1
-			};
-			moderator.createSensor(sensor,{
-				onSuccess:function(){
-					moderator.createSensor(sensor,{
-						onSuccess:function(){
-							done("Same sensor has been created twice");
-						},
-						onError:function(){
-							done();
-						}
-					});
-				},
-				onError:function(){
-					done("Failed to create first sensor");
-				}
-			});
-		});
-		it("createSensor failed : name undefined", function(done){
-			var sensor = {
-					type:Types.POSITIVE_NUMBER,
-					freq:1
-				};
-			moderator.createSensor(sensor,{
-				onSuccess:function(){
-					done("Sensor with no name has been created");
-				},
-				onError:function(){
-					done();
-				}
-			});
-		});
-		it("createSensor failed : type undefined", function(done){
-			var sensor = {
-					name:"test",
-					freq:1
-				};
-			moderator.createSensor(sensor,{
-				onSuccess:function(){
-					done("Sensor with no type has been created");
-				},
-				onError:function(){
-					done();
-				}
-			});
-		});
-		it("createSensor failed : freq undefined", function(done){
-			var sensor = {
-					name:"test",
-					type:Types.POSITIVE_NUMBER,
-				};
-			moderator.createSensor(sensor,{
-				onSuccess:function(){
-					done("Sensor with no freq has been created");
-				},
-				onError:function(){
-					done();
-				}
-			});
-		});
-		it("createSensor failed : not a moderator account", function(done){
-			var client = new Moderator(mqttUrl, simpleUser);
-			var sensor = {
-					name:"test",
-					type:Types.POSITIVE_NUMBER,
-					freq:1
-				};
-			client.createSensor(sensor,{
-				onSuccess:function(){
-					done();
-				},
-				onError:function(){
-					done("Simple user should not be able to create a sensor");
-				}
-			});
-		});
-		it("createSensor succeed", function(done){
-			moderator.createSensor({
-				name:"css",
-				type:Types.POSITIVE_NUMBER,
-				freq:1
-			},{
-				onSuccess:function(){
-					done();
-				},
-				onError:function(){
-					done("Failed to create a sensor");
-				}
-			});
-		});
-		it("deleteSensor failed : sensor does not exist", function(done){
-			moderator.deleteSensor("dsf",{
-				onSuccess:function(){
-					done("Succeed to delete an inexistant sensor");
-				},
-				onError:function(){
-					done();
-				}
-			});
-		});
-		it("deleteSensor successed", function(done){
-			moderator.createSensor({
-				name:"dss",
-				type:Types.POSITIVE_NUMBER,
-				freq:1
-			},{
-				onSuccess:function(){
-					moderator.deleteSensor("dss",{
-						onSuccess:function(){
-							done();
-						},
-						onError:function(){
-							done("Failed to delete a valid sensor");
-						}
-					});
-				},
-				onError:function(){
-					done("Failed to create a sensor");
-				}
-			});
-		});
-		it("deleteSensor failed : not a moderator account", function(done){
-			var client = new Moderator(mqttUrl, simpleUser);
-			moderator.createSensor({
-				name:"dsf_nama",
-				type:Types.POSITIVE_NUMBER,
-				freq:1
-			},{
-				onSuccess:function(){
-					client.deleteSensor("dsf_nama",{
-						onSuccess:function(){
-							done("Succeed to delete a sensor with a simple account");
-						},
-						onError:function(){
-							done();
-						}
-					});
-				},
-				onError:function(){
-					done("Failed to create a sensor");
-				}
-			});
-		});
-	});
+    describe('sensor management', function() {
+        before(function(done){
+            moderator = new Moderator(mqttUrl, moderatorUser);
+            moderator.on('connect', function(){
+                done();
+            });
+            moderator.on('error', function(){
+                done("Failed to initialize tests");
+            });
+        });
+        it("createSensor failed", function(done){
+            var sensor = {
+                name:"fail",
+                type:Types.POSITIVE_NUMBER,
+                freq:1
+            };
+            moderator.createSensor(sensor,{
+                onSuccess:function(){
+                    done("createSensor should call onError callback");
+                },
+                onError:function(){
+                    done();
+                }
+            });
+        });
+        it("createSensor succeed", function(done){
+            moderator.createSensor({
+                name:"succeed",
+                type:Types.POSITIVE_NUMBER,
+                freq:1
+            },{
+                onSuccess:function(){
+                    done();
+                },
+                onError:function(){
+                    done("createSensor should call onSuccess callback");
+                }
+            });
+        });
+        it("deleteSensor failed", function(done){
+            moderator.deleteSensor("fail",{
+                onSuccess:function(){
+                    done("deleteSensor should call onError callback");
+                },
+                onError:function(){
+                    done();
+                }
+            });
+        });
+        it("deleteSensor succeed", function(done){
+            moderator.deleteSensor("succeed",{
+                onSuccess:function(){
+                    done();
+                },
+                onError:function(){
+                    done("deleteSensor should call onSuccess callback");
+                }
+            });
+        });
+    });
 });
